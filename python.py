86df5429b991396defce3694c20f28eb234773cf
Pradeep Yadav <197392777+devopspradeepyadav@users.noreply.github.com>
2025-03-09 10:57:11 +0530

    Syntax error fixed

--- python.py
+++ python.py
@@ -1,3 +1,3 @@
 print ("Wassup buddy!! v1")
 print ("Wassup buddy!! v2 launched")
-print ("Checking if the email notification service is working!)
+print ("Checking if the email notification service is working!")
